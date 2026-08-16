d6bf6b802e02bf5f29630a0e89c8d7af72f8e541
Glitzie <glitziestudios@gmail.com>
2026-08-17 00:51:21 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://glitziestudios.github.io',
+	base: '/joule-docs',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Joule Documentation',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
